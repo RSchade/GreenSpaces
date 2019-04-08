b1a1c1385baccc72f37a04e6b587063423bc63f9
Raymond Schade <ray@kacluc.lu>
2019-04-08 16:46:46 +0300

    more sites

--- mapoverlays.js
+++ mapoverlays.js
@@ -1,7 +1,7 @@
 // initialize with toner-lite from stamen
 var layer = new L.StamenTileLayer("toner-lite");
 var map = new L.Map("map", {
-    center: new L.LatLng(40.63, 22.945),
+    center: new L.LatLng(40.635, 22.945),
     zoom: 15
 });
 map.addLayer(layer);
@@ -11,7 +11,8 @@ $.getJSON("airbnbcost.json", function(obj) {
 	var heat = L.heatLayer(obj, {radius: 15}).addTo(map);
 	layers.addOverlay(heat, "AirBnB Cost");
 });
-$.getJSON("greenSpaces.geojson", function(obj) {
+var greenFill = "#30db47";
+$.getJSON("greenSpaces_200m.geojson", function(obj) {
 	var highestAmt = 0;
 	obj.forEach(function(feature) {
 		if(highestAmt < feature.properties.greenSpaceM) {
@@ -30,14 +31,72 @@ $.getJSON("greenSpaces.geojson", function(obj) {
 	}).addTo(map);
 	layers.addOverlay(greenSpace, "Relative Green Space");
 });
+// create the icons for the markers
+var pinTypes = {
+	"hospital": "hospitalpin.svg",
+	"hotel": "hotelpin.svg",
+	"residential": "residentialpin.svg",
+	"school": "schoolpin.svg",
+	"abandoned": "abandonedpin.svg",
+	"commercial": "commercialpin.svg"
+}
+var pinTypeNames = ["Hospital Sites", "Hotel Sites", "Residential Sites",
+		    "School Sites", "Abandoned Sites", "Buisiness Sites"];
+Object.keys(pinTypes).forEach(function(pinName) {
+	var pin = L.icon({
+		iconUrl: `/icons/${pinTypes[pinName]}`,
+		iconSize: [30,30],
+		iconAnchor: [15,30],
+		popupAnchor: [0,0]
+	});
+	pinTypes[pinName] = pin;
+});
 // add the markers with the sites
 $.getJSON("newsites.json", function(obj) {
+	var siteLayers = {};
+	var siteKeys = Object.keys(pinTypes);
+	for(var i = 0; i < pinTypeNames.length; i++) {
+		var pinType = siteKeys[i];
+		siteLayers[pinType] = L.layerGroup();
+		siteLayers[pinType].addTo(map);
+		layers.addOverlay(siteLayers[pinType], pinTypeNames[i]);
+	}
 	obj.forEach(function(site) {
-		console.log(site);
-		var curMarker = L.marker(site.coord);
-		var curPopup = L.popup()
-			.setContent("<img width=300 src='"+site.img+"'><br><h3>"+site.title+"</h3>");
+		var options = {};
+		if(site.icon) {
+			options['icon'] = pinTypes[site.icon];
+		} else {
+			site.icon = "residential";
+		}
+		var curMarker = L.marker(site.coord, options);
+		siteLayers[site.icon].addLayer(curMarker);
+		var popupHTML =
+			`<h3 style="margin-bottom: 0">${site.title}</h3>
+			 <h6 style="margin: 0">${site.type}</h6>
+			 <img width=250 style="display: inline-block" src='${site.img}'>
+			 <p>${site.description}</p>`
+		var curPopup = L.popup({className: "greensite-popup"})
+			.setContent(popupHTML);
 		curMarker.bindPopup(curPopup);
 		curMarker.addTo(map);
 	});
 });
+// Add the legend onto the bottom right corner of the map
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function(map) {
+	var div = L.DomUtil.create('div', 'info legend');
+	div.innerHTML = `<i style="background: ${greenFill};"></i> Green Space Concentration
+                         <br>
+                         <i style="background-image: linear-gradient(to right, blue, green, yellow,  red);"></i> AirBnB Heatmap
+			 <br>
+			 <i style="background-image: url(/icons/residentialpin.svg);"></i> Residential
+			 <br>
+			 <i style="background-image: url(/icons/hospitalpin.svg);"></i> Hospital
+			 <br>
+			 <i style="background-image: url(/icons/hotelpin.svg);"></i> Hotel
+			 <br>
+			 <i style="background-image: url(/icons/schoolpin.svg);"></i> School
+			`;
+	return div;
+};
+legend.addTo(map);
